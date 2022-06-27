73b5175ad0003f9134ab0329ce85b4d849262a29
BrunoSobral <BrunoSobral@DESKTOP-RD9LI2N.mshome.net>
2022-06-27 20:19:25 -0300

    adicionando o campo idade da classe pessoa

--- projetojava1/src/curso/java/bruno/Pessoa.java
+++ projetojava1/src/curso/java/bruno/Pessoa.java
@@ -1,5 +1,29 @@
 package curso.java.bruno;
 
 public class Pessoa {
+	
+	private String nome;
+	private String sobrenome;
+	private int Idade;
+		
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 
 }
